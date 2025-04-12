fe620585ddebac057840e1e68ccc1ef34e182a4a
nguyenhai1508 <nguyenhaind0815@gmail.com>
2025-04-12 14:49:56 +0700

    init

---
+++ app/Http/Controllers/{{ cookiecutter.namespace_folder }}/{{ cookiecutter.controller_name }}Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\{{ cookiecutter.namespace_folder }};
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class {{ cookiecutter.controller_name }}Controller extends Controller
+{
+    public function index()
+    {
+        // TODO: return view or data
+    }
+
+    public function store(Request $request)
+    {
+        // TODO: store logic
+    }
+
+    public function update(Request $request, $id)
+    {
+        // TODO: update logic
+    }
+
+    public function destroy($id)
+    {
+        // TODO: delete logic
+    }
+}
