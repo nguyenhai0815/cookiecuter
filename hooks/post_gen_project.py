1d0b205ddfd69b5a1a6a7b120edd138efe2b5e2b
nguyenhai1508 <nguyenhaind0815@gmail.com>
2025-04-14 11:44:48 +0700

    init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,3 +12,23 @@ if "Controller" not in types:
 if "Service" not in types:
     service_path = os.path.join(base_path, "Services")
     shutil.rmtree(service_path, ignore_errors=True)
+
+model_name = "{{ cookiecutter.model_name }}".strip()
+namespace = "{{ cookiecutter.namespace_folder }}".strip()
+
+def run_artisan(command):
+    print(f"👉 Running: php artisan {command}")
+    try:
+        subprocess.run(f"php artisan {command}", shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Failed: {e}")
+
+# Tạo model + migration
+if model_name:
+    run_artisan(f"make:model {model_name} --migration")
+
+    # Tạo FormRequest
+    run_artisan(f"make:request Http/Requests/{namespace}/{model_name}Request")
+
+    # Tạo Resource
+    run_artisan(f"make:resource Http/Resources/{namespace}/{model_name}Resource")
