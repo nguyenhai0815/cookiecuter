c35c68cba16de2ba479977affefd02878861e55c
nguyenhai1508 <nguyenhaind0815@gmail.com>
2025-04-17 15:10:49 +0700

    wip

---
+++ resources/js/pages/{{ parent_module }}/{{ module_name }}/Index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>{{ module_name|capitalize }} Page ({{ parent_module }})</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ resources/js/stores/{{ parent_module }}/{{ module_name }}.js
@@ -0,0 +1,7 @@
+export const state = () => ({
+  // {{ module_name }} state for {{ parent_module }}
+})
+
+export const actions = {
+  // {{ module_name }} actions
+}
